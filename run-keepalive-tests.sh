cead4018eca7d685b7c6816d7c4917ce8361fc9f
Richard A. Lochner <lochner@clone1.com>
2024-10-02 10:43:17 -0500

    Added run-keepalive-tests.sh script.

---
+++ run-keepalive-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# run-keepalive-tests.sh: Run the keepalive compose file setting up the environment, if necessary.
+
+if [ "$(id -u)" != "0" ]; then
+	echo "Must bee root!"
+	exit 1
+fi
+
+if grep ^ip_vs /proc/modules >/dev/null 2>&1 ; then
+	echo "Info: ip_vs is already loaded!"
+else
+	echo "Info: Loading module ip_vs."
+	modprobe ip_vs
+fi
+
+if [ "$(cat /proc/sys/net/ipv4/ip_nonlocal_bind)" = "1" ]; then
+	echo "Info: net.ipv4.ip_nonlocal_bind is already set."
+else
+	echo "Info: Setting /proc/sys/net/ipv4/ip_nonlocal_bind to 1."
+	echo 1 >/proc/sys/net/ipv4/ip_nonlocal_bind
+fi
+
+podman build -t lochnerr/keepalived .
+
+podman volume rm -f keepalived_unittest
+podman compose -f docker-compose.test.yml up
+podman compose -f docker-compose.test.yml down
+podman volume rm -f keepalived_unittest
